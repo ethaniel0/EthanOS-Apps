5c514bcfd9e1e563a8c7f8e6ff0968626f82f487
Ethan Horowitz <ethan.horowitz@duke.edu>
2022-10-26 16:16:21 -0400

    Added Present app to server

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './Present.css';
+import { useState } from 'react';
+import LandingPage from './LandingPage';
+import ProjectView from './ProjectView';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [page, setPage] = useState(0);
+  const [filename, setFile] = useState("");
+    return (
+    <>
+        {
+            page === 0 ? 
+            <LandingPage setFile={(fname: string) => {setFile(fname); setPage(1)}} />
+            : 
+            <ProjectView filePath={filename} setPage={setPage} />
+        }
+    </>
+    );
 }
 
 export default App;
